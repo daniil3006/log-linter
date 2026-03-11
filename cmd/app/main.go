aba0e4bb1cb2ea91b835b4c99605fda7574d1946
daniil3006 <daniil.stank@gmail.com>
2026-03-11 22:18:01 +0300

    fix: main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"context"
 	"log-linter/pkg/analyzer"
-	"log/slog"
 
-	"go.uber.org/zap"
 	"golang.org/x/tools/go/analysis/singlechecker"
 )
 
 func main() {
 	singlechecker.Main(analyzer.Analyzer)
-	logger, _ := zap.NewProduction()
-	slog.Info("Server started")
-	slog.InfoContext(context.Background(), "Server start")
-	logger.Info("server started!")
 }
